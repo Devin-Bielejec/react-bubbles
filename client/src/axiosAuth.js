fb4b7f7b8e7ea211c9771a8c09bab09b885a0668
devin-bielejec <devin.bieleje@gmail.com>
2019-09-20 11:10:18 -0400

    Created axios with auth request

---
+++ client/src/axiosAuth.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem("token");
+
+    return axios.create({
+        header: {
+            "Content-Type": "application/json",
+            "Authorization": `${token}`
+        }
+    })
+}
